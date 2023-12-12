f6ed00063d9d9343814cde7dc50ef0d44986ecd5
tb <>
2023-12-12 17:33:48 +0100

    commit 2

--- src/TestPattern.java
+++ src/TestPattern.java
@@ -6,7 +6,6 @@ public class TestPattern {
         tweetCollection1.add(new Tweet("Salut !"));
         tweetCollection1.add(new Tweet("Mdr trop drole !"));
         tweetCollection1.add(new Tweet("Triple monstre !"));
-        PostCollection twitterCollection = tweetCollection1;
 
         for(PostIterator iter = tweetCollection1.createIterator(); iter.hasNext();){
             System.out.println(iter.getNext());
